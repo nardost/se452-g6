4d22b2dd80c71b663ed0042dbf815b8e2522f9b3
Pedro Rebollar <prebolla15@hotmail.com>
2020-04-19 10:08:54 -0500

    inde files

---
+++ accounts/src/main/java/edu/depaul/g6/accounts/AccountSubscription.java
@@ -0,0 +1,12 @@
+package edu.depaul.g6.accounts;
+
+public class AccountSubscription {
+
+    public void subcribe(){
+
+    };
+
+    public void unsubscribe(){
+
+    };
+}
---
+++ accounts/src/main/java/edu/depaul/g6/accounts/BillManagement.java
@@ -0,0 +1,12 @@
+package edu.depaul.g6.accounts;
+
+public class BillManagement {
+
+    public void viewBill(){
+
+    };
+
+    public void makePayment(){
+
+    };
+}
