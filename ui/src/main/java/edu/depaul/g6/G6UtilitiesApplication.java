bdb111ab0703bea2289e7579c2a413150a286f0e
nardos <32473919+nardost@users.noreply.github.com>
2020-04-21 13:55:14 -0500

    The SpringBootApplication is placed in package edu.edepaul.g6 to make sure that all components in packages that are under this package are located during component scanning.

---
+++ ui/src/main/java/edu/depaul/g6/G6UtilitiesApplication.java
@@ -0,0 +1,11 @@
+package edu.depaul.g6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G6UtilitiesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(G6UtilitiesApplication.class, args);
+    }
+}
