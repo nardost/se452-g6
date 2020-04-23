e84ab1fc8ba4fa5736f5cc4b0d3320704162305e
Pedro Rebollar <prebolla15@hotmail.com>
2020-04-22 20:54:23 -0500

    creating accountNumberGenerator

--- accounts/src/main/java/edu/depaul/g6/accounts/domain/Accounts.java
+++ accounts/src/main/java/edu/depaul/g6/accounts/domain/Accounts.java
@@ -2,10 +2,22 @@ package edu.depaul.g6.accounts.domain;
 
 public class Accounts {
 
+    private String accountNumber = "REU-";
+    private int number;
+    private String numberString;
+    
+    
     public void createAcccount(){
         /*
         This will create the initial profile for the user
         */
     }
-    
+
+    //ToDo: Need to find a way to create the first account number if none are in the system. If one is found, increment by one
+    public String accountNumberGenerator(){
+        number = 0000001;
+        numberString = Integer.toString(number);
+        System.out.println(accountNumber + numberString);
+        return accountNumber + numberString;
+    }
 }
