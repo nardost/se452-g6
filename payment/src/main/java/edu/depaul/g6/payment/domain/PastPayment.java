1e78b76fe80cfb3704757ca18772dab778c20b28
Dash323 <MatthewMorelandJames@gmail.com>
2020-05-02 20:07:58 -0500

    Add On Premise Monitor Simulator and Payments

---
+++ payment/src/main/java/edu/depaul/g6/payment/domain/PastPayment.java
@@ -0,0 +1,25 @@
+package edu.depaul.g6.payment.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table(name = "past_payments")
+public class PastPayment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer payment_id;
+
+    @Column(name = "customer_id")
+    private Integer customer_id;
+
+    @Column(name = "amount_paid")
+    private Integer cents;
+
+    @Column(name = "credit_card_num")
+    private String card_num;
+}
