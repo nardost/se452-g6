c71ca31b7fb559c9e56f249a9766750fe9adf5f9
Pedro Rebollar <prebolla15@hotmail.com>
2020-05-04 09:06:34 -0500

    adjusted insert.sql

--- accounts/src/main/resources/db/migration/V2.1__insert_values.sql
+++ accounts/src/main/resources/db/migration/V2.1__insert_values.sql
@@ -3,7 +3,7 @@ INSERT INTO account_management (full_name, email, password) VALUES
     ('John Doe', 'jdoe@gmail.com', 'password123'),
     ('Jake Fox', 'jfox@gmail.com', 'password12345');
 
-INSERT INTO employee_management (full_name, email, password) VALUES
+INSERT INTO employee_management (full_name, department, email, password) VALUES
     ('Alison Sorenson', 'Sales', 'asorenson@gmail.com', 'employeePassword1'),
     ('Jack Ryan', 'Support', 'jryan@gmail.com', 'employeepassword2'),
     ('Michael Jordan', 'Operations', 'mj@gmail.com', 'tempEmployeePassword3');
