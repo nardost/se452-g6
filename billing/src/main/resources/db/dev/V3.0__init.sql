a1edfa423e8f5d0cb5f0e8302e26046442932eb0
nardos <32473919+nardost@users.noreply.github.com>
2020-05-03 23:08:15 -0500

    SQL scripts edited to be compatible with production DB.

--- billing/src/main/resources/db/dev/V3.0__init.sql
+++ billing/src/main/resources/db/dev/V3.0__init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Bill (
     id SERIAL PRIMARY KEY,
-    utility_id VARCHAR(8) NOT NULL,
+    account_number VARCHAR(10) NOT NULL,
     billing_date DATE DEFAULT CURRENT_DATE,
     status BOOLEAN DEFAULT FALSE
 );
--- facilities/src/main/resources/db/prod/V1.0__init.sql
+++ facilities/src/main/resources/db/prod/V1.0__init.sql
@@ -1,8 +1,19 @@
-CREATE TABLE service_locations (
-    id SERIAL PRIMARY KEY,
-    street_address VARCHAR(255) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    state VARCHAR(2) NOT NULL,
-    zip_code INTEGER NOT NULL,
-    unit VARCHAR(16)
+-- service locations
+create table service_locations (
+    id serial primary key,
+    street_address varchar(255),
+    city varchar(50),
+    state varchar(2),
+    zip_code integer,
+    unit varchar(16),
+    meter_mac_address varchar(20)
+);
+
+-- subscriptions
+create table subscriptions (
+    id varchar(10) primary key,
+    location_id integer references service_locations,
+    service_category varchar(20),
+    activation_timestamp timestamp,
+    service_status varchar(10)
 );
