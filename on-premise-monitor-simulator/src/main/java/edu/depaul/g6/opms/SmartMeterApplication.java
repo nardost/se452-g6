c8ebf362c9b1eb11d2fb9b7fb207577fe0f75b69
nardos <32473919+nardost@users.noreply.github.com>
2020-05-13 12:16:42 -0500

    smart meter simulator is now a stand alone executable jar

---
+++ on-premise-monitor-simulator/src/main/java/edu/depaul/g6/opms/SmartMeterApplication.java
@@ -0,0 +1,11 @@
+package edu.depaul.g6.opms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMeterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMeterApplication.class, args);
+    }
+}
