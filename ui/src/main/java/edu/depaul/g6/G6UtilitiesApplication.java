f7b31bf898da7005a08eacadb7832e84168028f2
nardos <32473919+nardost@users.noreply.github.com>
2020-05-15 19:56:11 -0500

    Error handling started. Not completed yet.

--- ui/src/main/java/edu/depaul/g6/G6UtilitiesApplication.java
+++ ui/src/main/java/edu/depaul/g6/G6UtilitiesApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication(exclude = {
+        ErrorMvcAutoConfiguration.class,
         SecurityAutoConfiguration.class,
         ManagementWebSecurityAutoConfiguration.class
 })
