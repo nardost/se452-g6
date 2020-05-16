e68ba48d3d96f9911ebdae323b3487a99f6ac6b0
Christian Kleinvehn <ckleinvehn@gmail.com>
2020-05-16 15:08:31 -0500

    Got the 404.html working.

--- ui/src/main/java/edu/depaul/g6/G6UtilitiesApplication.java
+++ ui/src/main/java/edu/depaul/g6/G6UtilitiesApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-        ErrorMvcAutoConfiguration.class,
         SecurityAutoConfiguration.class,
         ManagementWebSecurityAutoConfiguration.class
 })
