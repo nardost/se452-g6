6606b5c9ef4a5973b661764613b24d86f48f3cbf
nardos <32473919+nardost@users.noreply.github.com>
2020-05-26 01:21:50 -0500

    Redis cloud connects to application. Scheduled task works.

--- ui/src/main/java/edu/depaul/g6/G6UtilitiesApplication.java
+++ ui/src/main/java/edu/depaul/g6/G6UtilitiesApplication.java
@@ -2,11 +2,14 @@ package edu.depaul.g6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRedisHttpSession
 public class G6UtilitiesApplication {
     public static void main(String[] args) {
         SpringApplication.run(G6UtilitiesApplication.class, args);
